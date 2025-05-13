0ed2d46cb98ae18cf001629e0d3d456ffc0375c2
MarioFilian <mafilian@uce.edu.ec>
2025-05-13 13:17:22 -0500

    cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors    = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());   // <<< esto habilita CORS para cualquier origen
 
 // La variable de entorno PLATFORM la definiremos en cada despliegue
 const platform = process.env.PLATFORM || "Local";
